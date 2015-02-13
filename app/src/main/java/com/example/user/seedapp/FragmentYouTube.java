eaf6134869668938d6d61c83562db2c255f8603e
Lacknoito <lac.noito@gmail.com>
2015-02-13 21:14:23 +0700

    fragment youtube

--- app/src/main/java/com/example/user/seedapp/FragmentYouTube.java
+++ app/src/main/java/com/example/user/seedapp/FragmentYouTube.java
@@ -34,7 +34,7 @@ public class FragmentYouTube extends Fragment {
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_youtube, container, false);
 
         YouTubePlayerSupportFragment youTubePlayerFragment = YouTubePlayerSupportFragment.newInstance();
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
