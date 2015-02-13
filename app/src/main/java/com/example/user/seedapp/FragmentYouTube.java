52752bcef16c3360aa6089f3e79874e453006ed6
Lacknoito <lac.noito@gmail.com>
2015-02-13 21:13:29 +0700

    fragment youtube

---
+++ app/src/main/java/com/example/user/seedapp/FragmentYouTube.java
@@ -0,0 +1,65 @@
+package com.example.user.seedapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+
+/**
+ * Created by LacNoito on 2/13/2015.
+ */
+public class FragmentYouTube extends Fragment {
+
+    private FragmentActivity myContext;
+    private YouTubePlayer YPlayer;
+    private static final String YoutubeDeveloperKey = "AIzaSyCjfgiAytO0iYrnz7EQuWarGLSSPmW_mw0";
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
+
+    @Override
+    public void onAttach(Activity activity) {
+
+        if (activity instanceof FragmentActivity) {
+            myContext = (FragmentActivity) activity;
+        }
+
+        super.onAttach(activity);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        YouTubePlayerSupportFragment youTubePlayerFragment = YouTubePlayerSupportFragment.newInstance();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.youtube_fragment, youTubePlayerFragment).commit();
+
+        youTubePlayerFragment.initialize(YoutubeDeveloperKey, new YouTubePlayer.OnInitializedListener() {
+
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider arg0, YouTubePlayer youTubePlayer, boolean b) {
+                if (!b) {
+                    YPlayer = youTubePlayer;
+                    YPlayer.setFullscreen(false);
+                    YPlayer.loadVideo("l8Iu9PEKMmw");
+                    YPlayer.play();
+                }
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider arg0, YouTubeInitializationResult arg1) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
+        return rootView;
+    }
+
+}
